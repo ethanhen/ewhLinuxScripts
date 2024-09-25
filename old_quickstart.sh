7283d1a0381e8e4aaac6f9499c0834fe5819393b
ethanhen <github@ethanhen.com>
2024-09-24 20:58:31 -0400

    THIS IS UNTESTED

--- old_quickstart.sh
+++ old_quickstart.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#quick setup script for getting files and settings to preference
 
-#check for file conflict before downloading
+# handle file conflict
 if [ -f .bashrc ]; then
+	mv .bashrc .bashrc.old
+fi
+
+if [ -f .bash_aliases ]; then
 	mv .bashrc .bashrc.old
 fi
 
@@ -11,45 +14,24 @@ if [ -f .vimrc ]; then
 	mv .vimrc .vimrc.old
 fi
 
-#get required files
-wget https://raw.githubusercontent.com/ethanhen/ewhLinuxScripts/main/graphicsfix.sh
-wget https://raw.githubusercontent.com/ethanhen/ewhLinuxScripts/main/remove.sh
+
+# get files
 wget https://raw.githubusercontent.com/ethanhen/ewhLinuxScripts/main/.bashrc
+wget https://raw.githubusercontent.com/ethanhen/ewhLinuxScripts/main/.bash_aliases
 wget https://raw.githubusercontent.com/ethanhen/ewhLinuxScripts/main/.vimrc
-wget https://raw.githubusercontent.com/GideonWolfe/Zathura-Pywal/master/genzathurarc
-wget https://raw.githubusercontent.com/GideonWolfe/Zathura-Pywal/master/zathura
-wget -O zathura-color.sh https://raw.githubusercontent.com/GideonWolfe/Zathura-Pywal/master/install.sh
 
-chmod +x graphicsfix.sh
-chmod +x remove.sh
-chmod +x zathura-color.sh
-
-#make sure packages are installed and updated
 apt-get -y update && apt-get upgrade
-apt-get -y install vim zathura python-is-python3 python3-pip python2 youtube-dl ffmpeg neofetch net-tools curl git latexmk
+apt-get -y vim fastfetch curl git net-tools fail2ban btop zip unzip ncdu
 
-#install other tools
-pip3 install pywal
-pip3 install gallery-dl
+# write ssh key
+read -p 'paste ssh public key: '
+echo "$REPLY" >> $HOME/.ssh/authorized_keys
+echo 'your authorized_keys file:\n '
+cat $HOME/.ssh/authorized_keys
 
-#VIM CONFIGURATIONS
+# disable password auth
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo echo 'PasswordAuthentication no' > /etc/ssh/sshd_config.d/50-cloud-init.conf
+
+# setup vimplugged
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir -p ~/.vim/UltiSnips && wget https://raw.githubusercontent.com/gillescastel/latex-snippets/master/tex.snippets && mv tex.snippets ~/.vim/UltiSnips/tex.snippets
-
-#run the zathura fix and the hyper-v settings fix
-/bin/bash zathura-color.sh
-/bin/bash graphicsfix.sh
-
-#ensure autologin is off
-sed -i '/AutomaticLoginEnable = true/c\#  AutomaticLoginEnable = true' /etc/gdm3/custom.conf
-sed -i '/AutomaticLogin = user1/c\#  AutomaticLogin = user1' /etc/gdm3/custom.conf
-
-#install vim plugins
-vim +PlugInstall +qall
-
-#setup reboot script
-chmod +x /etc/rc.local
-echo '/bin/bash ~/reboot.sh &' >> /etc/rc.local
-
-#shutdown
-shutdown -f -t 15
